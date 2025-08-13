a0a842506931c62b4956276cff7760a323dca7a4
harshal4040h <hbankar440@gmail.com>
2025-08-13 12:18:57 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Commn/Home';
 import Student_Res from './Commn/Student_Res';
 import BookHostel from './Commn/BookHostel';
-import H from './Commn/H';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -32,7 +32,7 @@ let allroutes = createBrowserRouter([
   }
 ])
 root.render(<RouterProvider router={allroutes} />);
-<H/>
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
